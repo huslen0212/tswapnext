180cfd36484902b4ec65700e71da16b1e4cec000
Zaya <azaya6076@gmail.com>
2025-03-28 12:07:09 +0800

    zaya

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import Footer from '@/components/Footer';
-import Header from '../components/Header';
-
+import Header from '@/components/Header';
+import Image from 'next/image';
+import styles from '../styles/face.module.css';
 export default function Home() {
   return (
     <div className="pageContainer">
       <Header />
       <div className="content">
-        nuur
+        <h1>Тасалбараа хялбараар борлуул</h1>
+
+        <div className={styles.searchContainer}>
+          <input type="text" placeholder="Хайлт хийнэ үү" />
+          <Image src="/photos/search.png" alt="Search Icon" width={24} height={24} className={styles.searchIcon} />
+        </div>
+
+        <div className={styles.categories}>
+          {["Бүх төрөл", "Концерт", "Спорт", "Купон", "Эвент", "Бусад"].map((category, index) => (
+            <button key={index}>{category}</button>
+          ))}
+        </div>
+
+        <h2>Таньд санал болгох тасалбарууд</h2>
       </div>
       <Footer />
     </div>
