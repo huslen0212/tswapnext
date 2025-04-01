e935b7042a5eee4e6c4247a70e044e74d7de3a76
huslen0212 <huslen.battogtoh@yahoo.com>
2025-04-01 14:31:35 +0800

    reg

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,10 +1,31 @@
-import Footer from '@/components/Footer';
-import Header from '../components/Header';
+import Image from 'next/image';
+import styles from '../styles/RegisterPage.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="pageContainer">
-        burtguuleh
+      <div className="content">
+        <div className={styles.leftside}>
+          <Image src="/photos/concert.png" alt="concert" width={700} height={240}/>
+          <Image src="/photos/movie.jpg" alt="movie" width={700} height={243}/>
+          <Image src="/photos/sport.jpg" alt="sport" width={700} height={243}/>
+        </div>
+
+        <div className={styles.rigthside}>
+            <h1>Бүртгүүлэх</h1>
+            <form className={styles.loginForm}>
+              <input type="text" id="username" placeholder="Овог" required />
+              <input type="text" id="username" placeholder="Нэр" required />
+              <input type="text" id="username" placeholder="Утасны дугаар" required />
+              <input type="text" id="username" placeholder="Цахим шуудан" required />
+              <input type="text" id="username" placeholder="Нэвтрэх нэр" required />
+              <input type="password" id="password" placeholder="Нууц үг" required />
+
+              <button type="submit">Бүртгүүлэх</button>
+            </form>
+        </div>
+      </div>
     </div>
   );
 }
