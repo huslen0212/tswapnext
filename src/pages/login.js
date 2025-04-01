6e1fe83e79983a5a80dce15422811e5009d0b5da
huslen0212 <huslen.battogtoh@yahoo.com>
2025-04-01 12:43:21 +0800

    login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,25 @@
-import Footer from '@/components/Footer';
-import Header from '../components/Header';
+import Image from 'next/image';
+import styles from '../styles/LoginPage.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="pageContainer">
-        nevtreh
+        <div className={styles.leftside}>
+          <Image src="/photos/concert.png" alt="concert" width={700} height={240}/>
+          <Image src="/photos/movie.jpg" alt="movie" width={700} height={243}/>
+          <Image src="/photos/sport.jpg" alt="sport" width={700}height={243}/>
+        </div>
+
+        <div className={styles.rigthside}>
+            <h1>Нэвтрэх</h1>
+            <form className={styles.loginForm}>
+              <input type="text" placeholder="Имэйл хаяг" required />
+              <input type="password" placeholder="Нууц үг" required />
+              <button type="submit">Нэвтрэх</button>
+            </form>
+            <p>Шинэ хэрэглэгч бол <Link href="/register">бүртгүүлэх</Link></p>
+        </div>
     </div>
   );
 }
