a40a7cad8004a12f99a49fbe43d47e2351bb1c2c
Zaya <azaya6076@gmail.com>
2025-04-03 16:29:00 +0800

    zaya

--- src/pages/addTicket.js
+++ src/pages/addTicket.js
@@ -1,12 +1,22 @@
 import Footer from '@/components/Footer';
 import Header from '../components/Header';
-
+import TicketPhoto from '@/components/TicketPhoto';
+import EventForm from '@/components/EventForm';
+import DescriptionForm from '@/components/DescriptionForm';
+import styles from '../styles/addTicket.module.css';
 export default function Home() {
   return (
     <div className="pageContainer">
       <Header />
-      <div className="content">
-       zarah tasalbar nemeh
+      <div className={styles.content}>
+       <section classNeme={styles.TicketForm}>
+       <h1 style={{ color: '#ff7022' }}>Борлуулах тасалбараа нэмнэ үү:</h1>
+         <TicketPhoto />
+         <EventForm />
+       </section>
+       <div className={styles.description}>
+        <DescriptionForm/>
+       </div>
       </div>
       <Footer />
     </div>
